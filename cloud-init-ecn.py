e26f3853bb2979b7db676b3ddfbae435633d51af
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-11-27 14:24:18 -0500

    initial import

---
+++ cloud-init-ecn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import os
+import sys
+import yaml
+import argparse
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('--cloud-config', '-f',
+            default='/var/lib/cloud/instance/cloud-config.txt')
+    return p.parse_args()
+
+def main():
+    opts = parse_args()
+    cfg = yaml.load(open(opts.cloud_config))
+
+    if 'puppet_ecn' in cfg:
+        print yaml.dump(cfg['puppet_ecn'],
+                default_flow_style=False)
+
+if __name__ == '__main__':
+    main()
+
+
